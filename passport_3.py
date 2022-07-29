f9e7eddfa696ab27831e9e8b71d12978e87e3f83
PatrickZheng0 <pzheng111@gmail.com>
2022-07-29 12:20:49 -0400

    shiftx and move up pid should work, working on shifty pid

--- passport_3.py
+++ passport_3.py
@@ -26,16 +26,16 @@ turn_pid = PID(0.25, 0, 0.001)
 turn_pid.set_sample_time(0.1)
 turn_pid.SetPoint = 416 # frame size ~ 832x832 pxls
 
-shift_pid = PID(0.1, 0, 0)
+shift_pid = PID(0.05, 0, 0)
 shift_pid.set_sample_time(0.1)
 shift_pid.SetPoint = 416 # frame size ~ 832x832 pxls
 
 img = frame_read.frame
 corners, ids, rejects = cv2.aruco.detectMarkers(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), arucoDict)
 
-move_pid = PID(0.01, 0, 0)
+move_pid = PID(0.1, 0.025, 0.02)
 move_pid.set_sample_time(0.1)
-move_pid.SetPoint = 400 # drone stays 400mm from wall
+move_pid.SetPoint = 700 # drone stays 400mm from wall
 
 check_corner = False
 check_turn = False
@@ -57,16 +57,12 @@ while True:
 
             if (current_time - prev_time > 12) and (move_up == False):
                 tello.send_rc_control(0, 0, 20, 0)
-                sleep(4)
+                sleep(1)
                 tello.send_rc_control(0, 0, 0, 0)
                 move_up = True
         
         tello.send_rc_control(0, 0, 0, 0)
         check_corner = True
-    
-    if len(corners) == 0:
-        print('no marker')
-        continue
 
     marker = Marker(id, corners)
 
@@ -107,34 +103,35 @@ while True:
 
     tello.send_rc_control(0, 0, 0, 0)
 
-    for i in range(50):
-        img = frame_read.frame
-        corners, ids, rejects = cv2.aruco.detectMarkers(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), arucoDict)
-        marker = Marker(id, corners)
+    #for i in range(50):
+    # img = frame_read.frame
+    # corners, ids, rejects = cv2.aruco.detectMarkers(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), arucoDict)
+    # marker = Marker(id, corners)
 
-        side_len = marker.get_avg_side_length()
-        move_feedback = marker.get_dist_to_marker(size, focal_length)
-        move_pid.update(move_feedback)
-        move_output = int(move_pid.output)
+    side_len = marker.get_avg_side_length()
+    move_feedback = marker.get_dist_to_marker(size, focal_length)
+    move_pid.update(move_feedback)
+    move_output = int(move_pid.output)
 
-        shiftx_feedback = marker.get_center()[0]
-        shift_pid.update(shiftx_feedback)
-        shiftx_output = int(shift_pid.output)
+    shiftx_feedback = marker.get_center()[0]
+    shift_pid.update(shiftx_feedback)
+    shiftx_output = int(shift_pid.output)
 
-        shifty_feedback = marker.get_center()[1]
-        shift_pid.update(shifty_feedback)
-        shifty_output = int(shift_pid.output)
+    shifty_feedback = marker.get_center()[1]
+    shift_pid.update(shifty_feedback)
+    shifty_output = int(shift_pid.output)
 
-        #no shifty_output and shiftx_output
-        # tello.send_rc_control(0, -move_output, 0, 0)
-        # sleep(0.1)
-        # tello.send_rc_control(0, 0, 0, 0)
-        print(marker.get_dist_to_marker(size, focal_length))
+    tello.send_rc_control(-shiftx_output, -move_output, -shifty_output, 0)
+    sleep(0.075)
+    tello.send_rc_control(0, 0, 0, 0)
+    print(marker.get_dist_to_marker(size, focal_length))
+    print('shiftx: ', 416 - marker.get_center()[0])
+    print('shifty: ', 416 - marker.get_center()[1])
 
-        if marker.get_dist_to_marker(size, focal_length) < 300:
-            tello.send_rc_control(0, 0, 0, 0)
-            tello.land()
-            break
+    if len(corners) == 0 or (marker.get_dist_to_marker(size, focal_length) < 300):
+        tello.send_rc_control(0, 0, 0, 0)
+        tello.land()
+        break
 
 #     #TODO move this statement
 #     #TODO fix get dist method
